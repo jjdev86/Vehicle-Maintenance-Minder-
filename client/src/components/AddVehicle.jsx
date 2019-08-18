6d107033c631a50813c8dc1a2df8f0eeb64e612e
Jose Valadez <vinmajj@gmail.com>
2019-08-18 16:13:36 -0700

    modify state properties to sigular nouns

--- client/src/components/AddVehicle.jsx
+++ client/src/components/AddVehicle.jsx
@@ -54,10 +54,10 @@ class AddVehicle extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      "car-years": '',
-      "car-makes": '',
-      "car-models": '',
-      "car-model-trims": '',
+      "car-year": '',
+      "car-make": '',
+      "car-model": '',
+      "car-model-trim": '',
       "car-mileage": '',
     }
   }
@@ -103,28 +103,28 @@ class AddVehicle extends Component {
                 </tr>
                 <tr>
                   <td>
-                    {/* <select id="car-years" name="car-years">
+                    {/* <select id="car-year" name="car-year">
                     <option value>---</option>
                   </select> */}
-                    <input id="car-years" name="car-years" value={this.state.year} onChange={(e) => this.onEventChange(e)}></input>
+                    <input id="car-year" name="car-year" value={this.state.year} onChange={(e) => this.onEventChange(e)}></input>
                   </td>
                   <td>
-                    {/* <select id="car-makes" name="car-makes">
+                    {/* <select id="car-make" name="car-make">
                     <option value>---</option>
                   </select> */}
-                    <input id="car-makes" name="car-makes" value={this.state.make} onChange={(e) => this.onEventChange(e)}></input>
+                    <input id="car-make" name="car-make" value={this.state.make} onChange={(e) => this.onEventChange(e)}></input>
                   </td>
                   <td>
-                    {/* <select id="car-models" name="car-models">
+                    {/* <select id="car-model" name="car-model">
                     <option value>---</option>
                   </select> */}
-                    <input id="car-models" name="car-models" value={this.state.model} onChange={(e) => this.onEventChange(e)}></input>
+                    <input id="car-model" name="car-model" value={this.state.model} onChange={(e) => this.onEventChange(e)}></input>
                   </td>
                   <td>
-                    {/* <select id="car-model-trims" name="car-model-trims">
+                    {/* <select id="car-model-trim" name="car-model-trim">
                     <option value>---</option>
                   </select> */}
-                    <input id="car-model-trims" name="car-model-trims" value={this.state.trim} onChange={(e) => this.onEventChange(e)}></input>
+                    <input id="car-model-trim" name="car-model-trim" value={this.state.trim} onChange={(e) => this.onEventChange(e)}></input>
                   </td>
                   <td>
                     <input id="car-mileage" name="car-mileage" value={this.state.mileage} onChange={(e) => this.onEventChange(e)}></input>
